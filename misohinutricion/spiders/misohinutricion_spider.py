d88b9fbb5c1362ee52f04049b7c7258f434f0a90
Nepali <nepali_123@Surendras-MacBook-Air.local>
2022-07-13 00:43:41 +0545

    remainig is the parsing

--- misohinutricion/spiders/misohinutricion_spider.py
+++ misohinutricion/spiders/misohinutricion_spider.py
@@ -27,6 +27,7 @@ class MisohinutricionSpiderSpider(scrapy.Spider):
         # check if it's product page
         product_name = response.xpath('//div[@class="product-name"]').extract_first()
         if next_page:
+            open('next_page.txt', 'a').write(next_page + '\n')
             yield scrapy.Request(next_page, callback=self.parse_page)
         elif product_list:
             self.logger.info("Skipped page %s", response.url)
@@ -46,7 +47,6 @@ class MisohinutricionSpiderSpider(scrapy.Spider):
                 yield scrapy.Request(product, callback=self.parse_page)
         else:
             self.logger.info(f'No more pages to crawl for {response.url}')
-            breakpoint()
             open('crawled_urls.txt', 'a').write(response.url + '\n')
 
         # TODO find the next page
@@ -54,6 +54,13 @@ class MisohinutricionSpiderSpider(scrapy.Spider):
 
     
     def parse_product(self, response):
+        breakpoint()
+        product_name = response.xpath("//div[@class='product-name']/h1/text()").extract_first()
+        brand_name = response.xpath("//*[@itemprop='brand']/text()").extract_first()
+        format = ''
+        normal_price = ''
+        discounted_price = ''
+        product_image_url = ''
         yield {
             'url': response.url,
             'name': response.xpath("//div[@class='product-name']/h1/text()").extract_first(),
